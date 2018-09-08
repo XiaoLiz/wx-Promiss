c72b7350f9a8ea2f06248aca636737d5cdf4c29c
hatLi <hatLi@gamil.com>
2018-09-08 16:44:40 +0800

    feat:add

---
+++ index.js
@@ -0,0 +1,17 @@
+export default function promisify(fn) {
+    return function(params = {}) {
+      // console.log(params, "promisify js files")
+
+      return new Promise((resolve, reject) => {
+        params.success = function(res) {
+          resolve(res)
+        }
+
+        params.fail = function(res) {
+          reject(res)
+        }
+
+        fn(params)
+      })
+    }
+  }
